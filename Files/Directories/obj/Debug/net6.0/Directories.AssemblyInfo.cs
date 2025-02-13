306b9c99e42d589640177a0e5eedd714c841bf88
Ajztyles <Ajsolomon62@gmail.com>
2025-02-13 17:29:11 +0100

    Directories

--- Files/Directories/obj/Debug/net6.0/Directories.AssemblyInfo.cs
+++ Files/Directories/obj/Debug/net6.0/Directories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Directories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c104209c7fd1bf8ac9dc767fc8e1c14a133a5f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cba8898c941efb8c51a728a4ff3ebae94426ab5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Directories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Directories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
