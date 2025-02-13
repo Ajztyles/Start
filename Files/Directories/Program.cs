a9ea64ff5e0a5733a22fcf94731d66cfbfffcc41
Ajztyles <Ajsolomon62@gmail.com>
2025-02-13 11:33:24 +0100

    Your commit message

---
+++ Files/Directories/Program.cs
@@ -0,0 +1,21 @@
+﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
+// Creating and Deleting Directories
+
+// Make sure the example file exists
+const string filename = "TestFile.txt";
+// 1: WriteAllText overwrites a file with the given content
+if (!File.Exists(filename)) {
+File.WriteAllText(filename, "Application Programming ");
+}
+// 3: AppendAllText adds text to an existing file
+File.AppendAllText(filename, "This is a 300 Level Second Semester Course. ");
+// 4: A FileStream can be opened and written to until closed
+using (StreamWriter sw = File.AppendText(filename)) {
+sw.WriteLine("Line 1");
+sw.WriteLine("Line 2");
+sw.WriteLine("Line 3");
+}
+// 2: ReadAllText reads all the content from a file
+string content;
+content = File.ReadAllText(filename);
+Console.WriteLine(content);
